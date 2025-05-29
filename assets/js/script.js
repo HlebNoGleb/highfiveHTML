2db9af51d28e19e72d1b96e8e5813197441fcc5e
Gunko Gleb <gynyax5@gmail.com>
2025-05-29 22:28:04 +0300

    add steps block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 const swiper = new Swiper('.swiper', {
   loop: true,
+  spaceBetween: 30,
   pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">'+ 0 + (index + 1) + "</span>";
-        },
-      },
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"><span class="num">'+ 0 + (index + 1) + "</span><span class='dot'></span></span>";
+    },
+  },
 });
